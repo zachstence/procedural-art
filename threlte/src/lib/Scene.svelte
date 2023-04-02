2127e4afef8a0b8916afef953bfce50dd7222a3f
Zachary Stence <zachstence@gmail.com>
2023-04-02 15:30:33 -0500

    feat: cube

--- threlte/src/lib/Scene.svelte
+++ threlte/src/lib/Scene.svelte
@@ -1,48 +1,29 @@
 <script>
-	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
+	import { Canvas, OrbitControls, T } from '@threlte/core'
 	import { spring } from 'svelte/motion'
-	import { degToRad } from 'three/src/math/MathUtils'
 
 	const scale = spring(1)
 </script>
 
-<div>
-	<Canvas>
-		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
-		</T.PerspectiveCamera>
 
-		<T.DirectionalLight castShadow position={[3, 10, 10]} />
-		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
-		<T.AmbientLight intensity={0.2} />
+<Canvas>
+    <T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
+        <OrbitControls target={{ y: 0.5 }} />
+    </T.PerspectiveCamera>
 
-		<!-- Cube -->
-		<T.Group scale={$scale}>
-			<T.Mesh position.y={0.5} castShadow let:ref>
-				<!-- Add interaction -->
-				<InteractiveObject
-					object={ref}
-					interactive
-					on:pointerenter={() => ($scale = 2)}
-					on:pointerleave={() => ($scale = 1)}
-				/>
+    <T.DirectionalLight position={[3, 10, 10]} />
+    <T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
+    <T.AmbientLight intensity={0.2} />
 
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color="#333333" />
-			</T.Mesh>
-		</T.Group>
+    <!-- Cube -->
+    <T.Group scale={$scale}>
+        <T.Mesh position.y={0.5} castShadow>
+            <T.BoxGeometry />
+            <T.MeshStandardMaterial color="#333333" />
+        </T.Mesh>
+    </T.Group>
+</Canvas>
 
-		<!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-			<T.CircleGeometry args={[3, 72]} />
-			<T.MeshStandardMaterial color="white" />
-		</T.Mesh>
-	</Canvas>
-</div>
 
 <style>
-	div {
-		height: 100%;
-		width: 100%;
-	}
 </style>
