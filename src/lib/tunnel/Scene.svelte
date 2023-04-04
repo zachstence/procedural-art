7f9859adb610322bf5cc55f7444991569ec1b91f
Zachary Stence <zachstence@gmail.com>
2023-04-04 09:47:55 -0500

    feat: camera moving through tube

--- src/lib/tunnel/Scene.svelte
+++ src/lib/tunnel/Scene.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import { Canvas, OrbitControls, T } from '@threlte/core'
-  import { BufferGeometry, EllipseCurve, LineBasicMaterial, QuadraticBezierCurve3, TubeGeometry, Vector3 } from 'three';
+  import { writable } from 'svelte/store';
+    import { BufferGeometry, DoubleSide, LineBasicMaterial, MeshStandardMaterial, Points, QuadraticBezierCurve3, TorusKnotGeometry, TubeGeometry, Vector3 } from 'three';
+
+    const TUNNEL_RADIUS = 0.5
+
+    const wireframeMaterial = new MeshStandardMaterial({ side: DoubleSide, wireframe: true })
 
     const curve = new QuadraticBezierCurve3(
         new Vector3(0, 0, 0),
@@ -8,13 +13,26 @@
         new Vector3(2, 2, 10),
     )
 
-    const tubeGeometry = new TubeGeometry(curve, 15, 0.5)
+    const tubeGeometry = new TubeGeometry(curve, 15, TUNNEL_RADIUS, 50, false)
+
+    const curvePos = writable(0)
+    $: cameraPos = curve.getPointAt($curvePos)
+    $: cameraTarget = curve.getPoint($curvePos + 0.1)
 </script>
 
 
+<div class="flex flex-col text-white">
+    <label>
+        Curve Position
+        <input class="w-96" type="range" min="0" max="1" step="0.01" bind:value={$curvePos} />
+        {$curvePos}
+    </label>
+</div>
+
+
 <Canvas>
-    <T.PerspectiveCamera makeDefault position={[0, 0, 0]} fov={24}>
-        <OrbitControls target={{ x: 0, y: 0, z: 1 }} />
+    <T.PerspectiveCamera makeDefault position={[cameraPos.x, cameraPos.y, cameraPos.z]} fov={24}>
+        <OrbitControls target={{ x: cameraTarget.x, y: cameraTarget.y, z: cameraTarget.z}} />
     </T.PerspectiveCamera>
 
     <!-- Axes -->
@@ -33,13 +51,7 @@
         ]} />
     </T.Group>
 
-    <!-- Lighting -->
-    <T.DirectionalLight position={[3, 10, 10]} />
-    <T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
-    <T.AmbientLight intensity={0.2} />
+    <T.AmbientLight intensity={1} />
 
-    <T.Mesh>
-        <T.MeshStandardMaterial color="#333" />
-        <T.Line args={[tubeGeometry]} />
-    </T.Mesh>
+    <T.Mesh args={[tubeGeometry, wireframeMaterial]} />
 </Canvas>
